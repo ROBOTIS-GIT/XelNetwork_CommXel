419c449ea6b6137ebff043eb516c0ce073be94e8
Kei <kkw@robotis.com>
2018-08-24 09:47:39 +0900

    Updated XelNetworkNode class.

--- commXel_app/src/ap/xel_net/xel_net_node.hpp
+++ commXel_app/src/ap/xel_net/xel_net_node.hpp
@@ -9,190 +9,79 @@
 #define XEL_NET_NODE_HPP_
 
 #include "ros2.hpp"
-#include <sensor_msgs/Imu.hpp>
-#include <sensor_msgs/BatteryState.hpp>
-#include <sensor_msgs/LaserScan.hpp>
-#include <std_msgs/Empty.hpp>
-#include <std_msgs/Bool.hpp>
 
-
-#define BATTERY_STATE_PUBLISH_FREQUENCY     200    //hz
-#define LASER_SCAN_PUBLISH_FREQUENCY        200    //hz
-#define IMU_PUBLISH_FREQUENCY               200    //hz
-
-
-
-
-void publishBatteryState(sensor_msgs::BatteryState* topic_msg);
-void subscribeBatteryState(sensor_msgs::BatteryState* topic_msg);
-
-void publishLaserScan(sensor_msgs::LaserScan* msg);
-void subscribeLaserScan(sensor_msgs::LaserScan* msg);
-
-void publishImu(sensor_msgs::Imu* msg);
-void subscribeImu(sensor_msgs::Imu* msg);
-
-void subscribeReset(std_msgs::Empty* msg);
-void subscribeLed(std_msgs::Bool* msg);
 
 namespace XelNetwork {
+
+typedef struct XelInfo
+{
+  uint8_t             xel_id;
+  uint32_t            data_type;
+  char                data_name[32];
+  ros2::MessagePrefix msg_type;
+  uint32_t            pub_interval_hz;
+  void*               p_data;
+  ros2::CallbackFunc  p_callback_func;
+} XelInfo_t;
+
 
 class XelNetworkNode : public ros2::Node
 {
 public:
-    XelNetworkNode()
+  XelNetworkNode()
   : Node()
   {
-    commxel_battery_pub_ = this->createPublisher<sensor_msgs::BatteryState>("commXel_battery");
-    this->createWallFreq(BATTERY_STATE_PUBLISH_FREQUENCY, (ros2::CallbackFunc)publishBatteryState, commxel_battery_pub_);
-    commxel_battery_sub_= this->createSubscriber<sensor_msgs::BatteryState>("commXel_battery", (ros2::CallbackFunc)subscribeBatteryState);
+  }
 
-    laser_scan_pub_ = this->createPublisher<sensor_msgs::LaserScan>("commXel_laser");
-    this->createWallFreq(LASER_SCAN_PUBLISH_FREQUENCY, (ros2::CallbackFunc)publishLaserScan, laser_scan_pub_);
-    laser_scan_sub_ = this->createSubscriber<sensor_msgs::LaserScan>("commXel_laser", (ros2::CallbackFunc)subscribeLaserScan);
+  template <typename MsgT>
+  bool createNewTopic(XelInfo_t* info)
+  {
+    bool ret = false;
 
-    imu_pub_ = this->createPublisher<sensor_msgs::Imu>("commXel_imu");
-    this->createWallFreq(IMU_PUBLISH_FREQUENCY, (ros2::CallbackFunc)publishImu, imu_pub_);
-    imu_sub_ = this->createSubscriber<sensor_msgs::Imu>("commXel_imu", (ros2::CallbackFunc)subscribeImu);
+    switch(info->msg_type)
+    {
+      case ros2::TOPICS_PUBLISH:
+        ros2::Publisher<MsgT>* p_pub = this->createPublisher<MsgT>((const char*)info->data_name);
+        this->createWallFreq(info->pub_interval_hz, info->p_callback_func, info->p_data, p_pub);
+        ret = p_pub!=NULL ? true:false;
+        break;
 
-    reset_sub_ = this->createSubscriber<std_msgs::Empty>("commXel_reset", (ros2::CallbackFunc)subscribeReset);
-    led_sub    = this->createSubscriber<std_msgs::Bool>("commXel_led", (ros2::CallbackFunc)subscribeLed);
+      case ros2::TOPICS_SUBSCRIBE:
+        ros2::Subscriber<MsgT>* p_sub = this->createSubscriber<MsgT>((const char*)info->data_name, info->p_callback_func, info->p_data);
+        ret = p_sub!=NULL ? true:false;
+        break;
+
+      case ros2::SERVICE_REQUEST:
+        break;
+
+      case ros2::SERVICE_RESPONSE:
+        break;
+
+      case ros2::SERVICE:
+        break;
+
+      case ros2::PARAMETER:
+        break;
+
+      case ros2::ACTION:
+        break;
+
+      default:
+        break;
+    }
+
+    return ret;
   }
 
 
 private:
-  ros2::Publisher<sensor_msgs::BatteryState>*  commxel_battery_pub_;
-  ros2::Subscriber<sensor_msgs::BatteryState>* commxel_battery_sub_;
 
-  ros2::Publisher<sensor_msgs::LaserScan>*     laser_scan_pub_;
-  ros2::Subscriber<sensor_msgs::LaserScan>*    laser_scan_sub_;
-
-  ros2::Publisher<sensor_msgs::Imu>*           imu_pub_;
-  ros2::Subscriber<sensor_msgs::Imu>*          imu_sub_;
-
-  ros2::Subscriber<std_msgs::Empty>*           reset_sub_;
-  ros2::Subscriber<std_msgs::Bool>*            led_sub;
 };
 
 } // namespace XelNetwork
 
 
-void publishBatteryState(sensor_msgs::BatteryState* msg)
-{
-  static float cell_voltage_data_[3];
 
-  strcpy(msg->header.frame_id, "CommXel BatteryState");
-  msg->header.stamp             = ros2::now();
-
-  msg->voltage                  = 1;
-  msg->current                  = 2;
-  msg->charge                   = 3;
-  msg->capacity                 = 4;
-  msg->design_capacity          = 5;
-  msg->percentage               = 6;
-
-  msg->power_supply_status      = 0;
-  msg->power_supply_health      = 1;
-  msg->power_supply_technology  = 2;
-
-  msg->present                  = true;
-  msg->cell_voltage             = cell_voltage_data_;
-  msg->cell_voltage_size        = sizeof(cell_voltage_data_)/sizeof(float);
-  for(uint32_t i = 0; i < msg->cell_voltage_size; i++)
-  {
-    msg->cell_voltage[i]        = 3.7;
-  }
-
-  strcpy(msg->location, "PowerXel");
-  strcpy(msg->serial_number, "123-456-789");
-}
-
-void subscribeBatteryState(sensor_msgs::BatteryState* msg)
-{
-
-}
-
-
-
-void publishLaserScan(sensor_msgs::LaserScan* msg)
-{
-  static float ranges_data_[8];
-  static float intensities_data_[8];
-
-  strcpy(msg->header.frame_id, "CommXel LaserScan");
-  msg->header.stamp     = ros2::now();
-
-  msg->angle_min        = 1;
-  msg->angle_max        = 2;
-  msg->angle_increment  = 3;
-  msg->time_increment   = 4;
-  msg->scan_time        = 5;
-  msg->range_min        = 6;
-  msg->range_max        = 7;
-
-  msg->ranges           = ranges_data_;
-  msg->ranges_size      = sizeof(ranges_data_)/sizeof(float);
-  for(uint32_t i = 0; i < msg->ranges_size; i++)
-  {
-    msg->ranges[i]      = (float)(micros()%128);
-  }
-
-  msg->intensities      = intensities_data_;
-  msg->intensities_size = sizeof(intensities_data_)/sizeof(float);
-  for(uint32_t i = 0; i < msg->intensities_size; i++)
-  {
-    msg->intensities[i] = (float)(micros()%128);
-  }
-}
-
-
-void subscribeLaserScan(sensor_msgs::LaserScan* msg)
-{
-
-}
-
-
-
-void publishImu(sensor_msgs::Imu* msg)
-{
-  strcpy(msg->header.frame_id, "CommXel IMU");
-  msg->header.stamp          = ros2::now();
-
-  msg->orientation.x         = 1;
-  msg->orientation.y         = 2;
-  msg->orientation.z         = 3;
-  msg->orientation.w         = 4;
-  msg->angular_velocity.x    = 5;
-  msg->angular_velocity.y    = 6;
-  msg->angular_velocity.z    = 7;
-  msg->linear_acceleration.x = 8;
-  msg->linear_acceleration.y = 9;
-  msg->linear_acceleration.z = 10;
-}
-
-
-void subscribeImu(sensor_msgs::Imu* msg)
-{
-
-}
-
-
-void subscribeReset(std_msgs::Empty* msg)
-{
-
-}
-
-void subscribeLed(std_msgs::Bool* msg)
-{
-  if(msg->data == true)
-  {
-    ledOn(_DEF_LED1);
-  }
-  else
-  {
-    ledOff(_DEF_LED1);
-  }
-}
 
 
 #endif /* XEL_NET_NODE_HPP_ */
